5f2e12560b557b164153ba4d9a541000271f5682
Berkeley True <berkeleytrue@users.noreply.github.com>
2021-04-17 13:12:38 -0700

    feat: add spines

--- build/main.scad
+++ build/main.scad
@@ -1,20 +1,40 @@
 $fn = 50;
-minkowski () {
-  linear_extrude (height=1.5, center=true){
-    difference () {
-      union () {
-        square ([15.5, 12.0], center=true);
-        translate ([0, 10, ]) {
-          square ([5.5, 10.3], center=true);
+union () {
+  minkowski () {
+    linear_extrude (height=1.5, center=true){
+      difference () {
+        union () {
+          square ([15.5, 12.0], center=true);
+          translate ([0, 10, ]) {
+            square ([5.5, 10.3], center=true);
+          }
+        }
+        translate ([2.75, 3.5, ]) {
+          square ([1.68, 2.75]);
+        }
+        translate ([-4.43, 3.5, ]) {
+          square ([1.68, 2.75]);
         }
       }
-      translate ([2.75, 3.5, ]) {
-        square ([1.68, 2.75]);
+    }
+    sphere (r=0.25);
+  }
+  union () {
+    translate ([-5.2, 1.76, 0.75]) {
+      hull () {
+        sphere (r=0.7);
+        translate ([10.4, 0, 0]) {
+          sphere (r=0.7);
+        }
       }
-      translate ([-4.43, 3.5, ]) {
-        square ([1.68, 2.75]);
+    }
+    translate ([-5.2, -3.125, 0.75]) {
+      hull () {
+        sphere (r=0.7);
+        translate ([10.4, 0, 0]) {
+          sphere (r=0.7);
+        }
       }
     }
   }
-  sphere (r=0.25);
 }
--- src/gopro_flare/core.clj
+++ src/gopro_flare/core.clj
@@ -2,7 +2,18 @@
   (:require [scad-clj.model :as m]
             [scad-tarmi.maybe :as maybe]))
 
-(defn flare-mount[]
+(defn spline [spine-length]
+  (let [sphere-r 0.7
+        f-spine-length (- spine-length (* 2 sphere-r))]
+    (maybe/translate
+      [sphere-r 0 0]
+      (m/hull
+        (m/sphere sphere-r)
+        (maybe/translate
+          [f-spine-length 0 0]
+          (m/sphere sphere-r))))))
+
+(defn flare-mount []
   (let [flareMountHeight 16
         flareMountWidth 12.5
         flareMountThickness 2
@@ -11,30 +22,47 @@
         tabExtrusionHeight 10.3
         cutOutThickness 1.68
         cutOutDepth 2.75
-        chamfer 0.5]
+        chamfer 0.5
+        spine-length 11.8]
 
-    (m/minkowski
-      (m/extrude-linear {:height (- flareMountThickness chamfer)}
-        (maybe/difference
-          ; main platform
-          (maybe/union
-            (m/square (- flareMountHeight chamfer) (- flareMountWidth chamfer))
-            ; tab extrusion
+    (maybe/union
+      (m/minkowski
+        (m/extrude-linear {:height (- flareMountThickness chamfer)}
+          (maybe/difference
+            ; main platform
+            (maybe/union
+              (m/square (- flareMountHeight chamfer) (- flareMountWidth chamfer))
+              ; tab extrusion
+              (maybe/translate
+                [0
+                 10]
+                (m/square tabExtrusionWidth tabExtrusionHeight)))
+            ; cutouts
             (maybe/translate
-              [0
-               10]
-              (m/square tabExtrusionWidth tabExtrusionHeight)))
-          ; cutouts
-          (maybe/translate
-            [(/ tabExtrusionWidth 2)
-             (- (/ flareMountWidth 2) cutOutDepth)]
-            (m/square cutOutThickness cutOutDepth :center false))
-          (maybe/translate
-            [(- (- 0 cutOutThickness) (/ tabExtrusionWidth 2))
-             (- (/ flareMountWidth 2) cutOutDepth)]
-            (m/square cutOutThickness cutOutDepth :center false))))
-      (m/sphere (/ chamfer 2)))))
+              [(/ tabExtrusionWidth 2)
+               (- (/ flareMountWidth 2) cutOutDepth)]
+              (m/square cutOutThickness cutOutDepth :center false))
+            (maybe/translate
+              [(- (- 0 cutOutThickness) (/ tabExtrusionWidth 2))
+               (- (/ flareMountWidth 2) cutOutDepth)]
+              (m/square cutOutThickness cutOutDepth :center false))))
+        (m/sphere (/ chamfer 2)))
+      ;; splines
+      (let [spline-z (/ (- flareMountThickness chamfer) 2)
+            spline-x (- (/ (- flareMountHeight spine-length) 2) (/ flareMountHeight 2))]
 
+        (maybe/union
+          (maybe/translate
+            [spline-x
+             1.76
+             spline-z]
+            (spline spine-length))
+
+          (maybe/translate
+            [spline-x
+             (- (/ flareMountWidth 4))
+             spline-z]
+            (spline spine-length)))))))
 
 (defn main []
   [(m/fn! 50)
